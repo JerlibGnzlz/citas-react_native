8b0526b477bcba67830adce34a89d1b1a3f1a8b8
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-02-16 22:01:35 -0300

    style al paciente

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -6,37 +6,53 @@ const Pacierntes = ({ item }) => {
   const { paciente, fecha } = item;
 
   const formatearFecha = fecha => {
-    const nuevaFecha = new Date(fecha)
+    const nuevaFecha = new Date(fecha);
     const opciones = {
       weekday: "long",
       year: "numeric",
       month: "long",
       day: "numeric"
-    }
+    };
 
-    return nuevaFecha.toLocaleDateString("es-ES", opciones)
-  }
+    return nuevaFecha.toLocaleDateString("es-ES", opciones);
+  };
 
 
   return (
 
-    <View>
-      <Text style={styles.noPacientes}>
+    <View style={styles.contenedor}>
+      <Text style={styles.label}>Paciente:</Text>
+      <Text style={styles.texto}>
         {paciente}
       </Text>
-      <Text>{formatearFecha(fecha)}</Text>
+      <Text style={styles.fecha}>{formatearFecha(fecha)}</Text>
     </View>
   );
 };
 
 
 const styles = StyleSheet.create({
-  noPacientes: {
-    marginTop: 40,
-    textAlign: "center",
-    fontSize: 24,
-    fontWeight: "600",
-    color: "black"
+  contenedor: {
+    backgroundColor: "white",
+    padding: 20,
+    borderBlockColor: "#94a3b8",
+    borderBottomWidth: 1
+  }, label: {
+    color: "#374151",
+    textTransform: "uppercase",
+    fontWeight: 700,
+    marginBottom: 10
+  },
+  texto: {
+    color: "#6d28d9",
+    fontSize: 20,
+    fontWeight: 700,
+    marginBottom: 10
+  },
+  fecha: {
+    color: "#374151",
+
+
   }
 });
 
